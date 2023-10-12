1c9cb7eb648dcaa6d216717674b2931e3a2e4392
yangjian <jian.yang@rokid.com>
2023-10-12 17:25:21 +0800

    [add] add transform sample

---
+++ app/src/main/cpp/sample/basic/TransformSample.cpp
@@ -0,0 +1,30 @@
+//
+// Created by jian.yang on 2023/10/12.
+//
+
+#include "TransformSample.h"
+#include "../../util/GLUtils.h"
+#include "../../util/AndroidDebug.h"
+#include "../../util/stb_image.h"
+
+namespace Transform {
+
+    TransformSample::TransformSample() {
+        LOGD("###### TransformSample init");
+        mTexture1Id = 0;
+    }
+
+    TransformSample::~TransformSample() {}
+
+    void TransformSample::OnCreate() {
+
+    }
+
+    void TransformSample::OnDraw(int width, int height) {
+
+    }
+
+    void TransformSample::OnDestroy() {
+
+    }
+} // Transform
---
+++ app/src/main/cpp/sample/basic/TransformSample.h
@@ -0,0 +1,31 @@
+//
+// Created by jian.yang on 2023/10/12.
+//
+
+#ifndef OPENGLES_TUTORIALS_TRANSFORMSAMPLE_H
+#define OPENGLES_TUTORIALS_TRANSFORMSAMPLE_H
+
+#include "../BaseSample.h"
+
+namespace Transform {
+
+    class TransformSample : public BaseSample {
+    public:
+        TransformSample();
+
+        virtual ~TransformSample();
+
+        virtual void OnCreate();
+
+        virtual void OnDraw(int width, int height);
+
+        virtual void OnDestroy();
+
+    private:
+        GLuint mTexture1Id,mTexture2Id;
+        GLuint mVBO, mEBO, mVAO;
+    };
+
+} // Transform
+
+#endif //OPENGLES_TUTORIALS_TRANSFORMSAMPLE_H
